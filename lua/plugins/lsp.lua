bf670857eb5dd26e92b1ea21ebe6d68bda9e313c
Daniel Eichinger <deichinger0@gmail.com>
2023-04-09 14:54:23 +0200

    fix: Autocomplete key binding

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,6 +6,20 @@ lsp.on_attach(function(client, bufnr)
     lsp.buffer_autoformat()
 end)
 
+-- Change some key mappings e.g., Enter to confirm autocomplete
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
